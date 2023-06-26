359299a21b6eacd673b555ed39ffd85aee068b30
Juan <113471575+juanpj17@users.noreply.github.com>
2023-06-25 23:21:49 -0400

    Update time.c

--- Proyecto/Problema 4/time.c
+++ Proyecto/Problema 4/time.c
@@ -8,30 +8,41 @@ int main(int argc, char *argv[]) {
     int i;
     char programa[1000];
     double start, end;
-    // verifica si se proporcionó al menos un argumento al programa
-    if (argc > 1) { 
-        // recorre los argumentos del programa a partir del segundo
-        for (i = 2; i < argc; i++) { 
-            // agrega el argumento actual al nombre del programa
-            strcat(programa, argv[i]); 
-            // agrega un espacio después del argumento
-            strcat(programa, " "); 
+
+    // Verifica si se proporcionó al menos un argumento al programa
+    if (argc > 1) {
+        // Recorre los argumentos del programa a partir del segundo
+        for (i = 2; i < argc; i++) {
+            // Agrega el argumento actual al nombre del programa
+            strcat(programa, argv[i]);
+            // Agrega un espacio después del argumento
+            strcat(programa, " ");
         }
-        // imprime el nombre completo del programa
-        printf("%s\n", programa); 
-        // inicia el temporizador
-        start = clock(); 
-        // construye un comando para ejecutar el programa con Python 3
-        char command[1000] = "python3 "; 
-        // agrega el nombre del programa al comando
-        strcat(command, programa); 
-        // ejecuta el comando para ejecutar el programa
-        system(command); 
-         // detiene el temporizador
+
+        // Imprime el nombre completo del programa
+        printf("%s\n", programa);
+
+        // Inicia el temporizador
+        start = clock();
+
+        // Construye un comando para ejecutar el programa con c-es-horrible
+        char command[1000] = "c-es-horrible";
+        // Agrega el nombre del programa al comando
+        strcat(command, programa);
+        // Ejecuta el comando para ejecutar el programa
+        system(command);
+
+        // Detiene el temporizador
         end = clock();
-        // imprime el tiempo de ejecución del programa
-        printf("%f segundos\n", (end - start) / CLOCKS_PER_SEC); 
+
+        // Imprime el tiempo de ejecución del programa
+        printf("%f segundos\n", (end - start) / CLOCKS_PER_SEC);
+    } else {
+        // Indica que no se proporcionaron suficientes argumentos al programa
+        printf("ERROR en los parametros\n");
     }
+    return 0;
+}
     else {
         // indica que no se proporcionaron suficientes argumentos al programa
         printf("ERROR en los parametros\n"); 
